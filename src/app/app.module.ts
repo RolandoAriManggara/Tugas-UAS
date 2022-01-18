b05c508994607785ce272016f5395118b517652a
RolandoAriManggara <qyarafisa08@gmail.com>
2022-01-18 15:55:53 +0700

    Tugas UAS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { DivisionComponent } from './divisions/divisions.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMenuComponent,
+    AppFooterComponent,
+    DivisionComponent,
+    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/divisions.model.spec.ts
@@ -0,0 +1,7 @@
+import { Division } from './divisions.model';
+
+describe('Divisions', () => {
+  it('should create an instance', () => {
+    expect(new Division()).toBeTruthy();
+  });
+});
